f395a274562a69a31ee23af60224ce91dc1e8815
omkarvinayaka1209 <omkarvinayaka1209@gmail.com>
2026-04-17 11:07:11 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,9 +5,9 @@ import App from './App';
 import './assets/css/style.css';
 
 // Make backend URL always available to legacy pages
-window.VEMU_SERVER_API_BASE = 'http://localhost:4000';
-window.VEMU_SMS_API_BASE = 'http://localhost:4000';
-localStorage.setItem('vemu_sms_api_base', 'http://localhost:4000');
+window.VEMU_SERVER_API_BASE = 'https://library-e5q2.onrender.com';
+window.VEMU_SMS_API_BASE = 'https://library-e5q2.onrender.com';
+localStorage.setItem('vemu_sms_api_base', 'https://library-e5q2.onrender.com');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
