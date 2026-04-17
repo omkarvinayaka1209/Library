88190fdae138cf63e76a1f0cc24d998930fd8336
omkarvinayaka1209 <omkarvinayaka1209@gmail.com>
2026-04-17 11:06:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:4000';
+const API_BASE = 'https://library-e5q2.onrender.com';
 
 export async function fetchLibraryState() {
   const res = await fetch(`${API_BASE}/api/library-state`);
